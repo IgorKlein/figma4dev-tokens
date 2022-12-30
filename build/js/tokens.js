1439891bcbe940e0a83b7868fe33222473406a73
Igor Klein <igor.klein.sl@gmail.com>
2022-12-30 12:17:44 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 30 Dec 2022 15:12:35 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorPurple500": "#8257e6",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f"
+};
